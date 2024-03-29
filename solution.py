cf0ed5589a26b502c6ea498501cfea9d6420d239
000Yuriy000 <109694063+000Yuriy000@users.noreply.github.com>
2024-03-29 11:40:46 +0300

    Модуль 1 задание 8

---
+++ solution.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+n = int(input())
+max_values =[]
+for _ in range(n):
+    record = input()
+    values = record.split()
+    values = [int(values) for values in values]
+    max_value = max(values)
+    max_values.append(max_value)
+    
+max_values.sort(reverse=True)
+output =';'.join(map(str, max_values))
+print(output)
